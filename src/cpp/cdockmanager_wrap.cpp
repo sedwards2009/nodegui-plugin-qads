18879d116fa72605a9cb26ae3ac86468496decaf
Simon Edwards <simon@simonzone.com>
2023-06-17 12:09:38 +0200

    Put back a "load bearing" do nothing C++ destructor.

--- src/cpp/cdockmanager_wrap.cpp
+++ src/cpp/cdockmanager_wrap.cpp
@@ -30,6 +30,8 @@ Napi::Object CDockManagerWrap::init(Napi::Env env, Napi::Object exports) {
 
 ads::CDockManager* CDockManagerWrap::getInternalInstance() { return this->instance; }
 
+CDockManagerWrap::~CDockManagerWrap() { }
+
 CDockManagerWrap::CDockManagerWrap(const Napi::CallbackInfo& info)
     : Napi::ObjectWrap<CDockManagerWrap>(info) {
   Napi::Env env = info.Env();
